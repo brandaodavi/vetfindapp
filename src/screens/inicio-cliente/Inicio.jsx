a2ea8000d5e8b3acc3b9cce7c7bc567f02e9a0c8
Davi Brandao <davi3388@outlook.com>
2023-12-26 21:52:49 -0300

    Implementacao do mapa

--- src/screens/inicio-cliente/Inicio.jsx
+++ src/screens/inicio-cliente/Inicio.jsx
@@ -1,3 +1,5 @@
+import { StyleSheet } from "react-native";
+import MapView from "react-native-maps";
 import { SafeAreaView } from "react-native-safe-area-context";
 
 import { Text } from "../../components";
@@ -5,8 +7,23 @@ const Inicio = () => {
 	return (
 		<SafeAreaView>
 			<Text>Inicio</Text>
+			<MapView
+				style={styles.map}
+				initialRegion={{
+					latitude: -19.90,
+					longitude: -43.96,
+					latitudeDelta: 0.0922,
+					longitudeDelta: 0.0421,
+				}}
+			></MapView>
 		</SafeAreaView>
 	);
 };
 
+const styles = StyleSheet.create({
+	map: {
+		width: "100%",
+		height: "100%",
+	},
+});
 export default Inicio;
