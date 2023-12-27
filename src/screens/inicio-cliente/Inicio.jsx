f5eec8916d0bb6dfbbb4a5c56ecb25f9bc3463f1
Davi Brandao <davi3388@outlook.com>
2023-12-26 22:41:48 -0300

    Permissão de acesso a localizacao + Configuracao Mapa com localizacao do usuário

--- src/screens/inicio-cliente/Inicio.jsx
+++ src/screens/inicio-cliente/Inicio.jsx
@@ -1,20 +1,40 @@
+import * as Location from "expo-location";
+import * as Permissions from "expo-permissions";
+import { useEffect, useRef, useState } from "react";
 import { StyleSheet } from "react-native";
 import MapView from "react-native-maps";
 import { SafeAreaView } from "react-native-safe-area-context";
 
 import { Text } from "../../components";
 const Inicio = () => {
+	const [origem, setOrigem] = useState(null);
+	const [destino, setDestino] = useState(null);
+
+	useEffect(() => {
+		(async function () {
+			const { status, permissions } = await Permissions.askAsync(Permissions.LOCATION);
+			if (status === "granted") {
+				let localizacao = await Location.getCurrentPositionAsync({ enableHighAccuracy: true });
+				setOrigem({
+					latitude: localizacao.coords.latitude,
+					longitude: localizacao.coords.longitude,
+					latitudeDelta: 0.00922,
+					longitudeDelta: 0.00421,
+				});
+			} else {
+				throw new Error("Localização não fornecida");
+			}
+		})();
+	}, []);
+
 	return (
 		<SafeAreaView>
 			<Text>Inicio</Text>
 			<MapView
 				style={styles.map}
-				initialRegion={{
-					latitude: -19.90,
-					longitude: -43.96,
-					latitudeDelta: 0.0922,
-					longitudeDelta: 0.0421,
-				}}
+				initialRegion={origem}
+				showsUserLocation={true}
+				loadingEnabled={true}
 			></MapView>
 		</SafeAreaView>
 	);
